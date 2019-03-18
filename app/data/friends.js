5bdd0ae4eb1e4cd010c50750a8bc0bbcf20839e2
Andromeda <hannalauth@gmail.com>
2019-03-17 18:08:50 -0700

    progress with posting

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,20 @@
-// This file will hold all submitted survey results
+// Dummy array
+
+var friendsList = [
+    {
+        name: "Hanna Lauth",
+        photoLink: "https://www.facebook.com/photo.php?fbid=10156460922233072&set=a.454677313071&type=3&theater",
+        q1: "1 (Strongly Disagree)",
+        q2: "2",
+        q3: "3",
+        q4: "4",
+        q5: "4",
+        q6: "4",
+        q7: "3",
+        q8: "2",
+        q9: "2",
+        q10: "1 (Strongly Disagree)"
+    }
+]
+
+module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,31 +7,34 @@
 // These data sources hold arrays of the survey result information.
 // ===============================================================================
 
-var friendsList = require("../data/friends");
+var friendsData = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsList);
-  });
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+    });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // When a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // When a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
 
-//   app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function(req, res) {
+        console.log("Req.body", req.body);
+        friendsData.push(req.body)
+    });
 
   };
